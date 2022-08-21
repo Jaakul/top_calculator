b3342ab14f1aa59afa9f08ba52c9333b9039d10b
Jaakul <jackw1689@gmail.com>
2022-08-20 20:27:34 -0700

    need to reprogram operator functions

--- javascript.js
+++ javascript.js
@@ -18,78 +18,92 @@ const nine = document.getElementById('nine');
 const zero = document.getElementById('zero');
 const equals = document.getElementById('equals');
 
-
-
-
-
-
-
-
 let calc = {$1st:'',$op:'',$2nd:''};
-let result = 0;
+let result = ''
 let currentDisplay = document.getElementById('display');
 let num1
 let num2
 
+const keyRegEx = /[0-9]/
 
-function pushToDisplay(){
-
-}
-
-function opCheck(){
+function operatorEmpty(){
 if (typeof calc.$op == 'function'){
     return false
 }
 return true
 }
 
-
 function isEmpty(e){
       for (let key in e){
         return false
     }  
-    return true;
-
-
-
+    return true
 }
+
+
+//**function to check if number exceeds window space */
+function displaySplitter(){
+    if(currentDisplay.textContent.includes('e')){
+        if(currentDisplay.textContent.length<9){
+            return currentDisplay.textContent
+        }
+        let x
+        let y
+        x = currentDisplay.textContent.slice(0,5)
+        y = currentDisplay.textContent.slice(-4)
+        return currentDisplay.textContent = x+y;
+        
+    }
+    if(currentDisplay.textContent.length>9){
+        let x
+        x = (currentDisplay.textContent).slice(0,9);
+        console.log(x.length);
+        return currentDisplay.textContent = x
+    }
+    }
+
 
 //all operator functions
 let calcAdd = function(){
-    a = parseInt(calc.$1st)
-    b = parseInt(calc.$2nd)
+    a = parseFloat(calc.$1st)
+    b = parseFloat(calc.$2nd)
     result = a+b
     calc.$1st = result;
+    displaySplitter()
        return result
-
     }
+
 let calcSubtract = function(){
-    a = parseInt(calc.$1st)
-    b = parseInt(calc.$2nd)
+    a = parseFloat(calc.$1st)
+    b = parseFloat(calc.$2nd)
     result = a-b;
     calc.$1st = result;
+    displaySplitter()
     return result
     }
     
 let calcMulti = function(){
-        a = parseInt(calc.$1st)
-        b = parseInt(calc.$2nd)
+        a = parseFloat(calc.$1st)
+        b = parseFloat(calc.$2nd)
            result = a*b;
            calc.$1st = result;
+           displaySplitter()
            return result
-        }
+    }
 let calcDiv = function(){
-            a = parseInt(calc.$1st)
-            b = parseInt(calc.$2nd)
-            if (b===0){
-                return result = "ERROR"            
-            }
-            else{
-               result = a/b
-               calc.$1st = result;
-               return result
-            }
-            }
+    a = parseFloat(calc.$1st)
+    b = parseFloat(calc.$2nd)
+        if (b===0){
+            return result = "ERROR"            
+        }
+        else{
+            result = a/b
+            calc.$1st = result;
+            displaySplitter()
+            return result
+        }
+
+    }
 
 //clear function
 function calcClear(){
@@ -102,46 +116,233 @@ function calcClear(){
 }
 
 
-
-//runs the current operation stored in calculator.operator         
+//EQUALS runs the current operation stored in calculator.operator         
 function calcEquals(){
    calc.$op()
-   return currentDisplay.textContent = result;
+   currentDisplay.textContent = result;
+   displaySplitter();
+   console.table(calc);
 }
 
-function numInput(e){
 
-    if (opCheck(calc.$op) && isEmpty(calc.$2nd)){
+//inputs number into object key 1 or 2 depending on operator presence
+function numInput(){
+    e=this.textContent;
+    if (operatorEmpty(calc.$op) && isEmpty(calc.$2nd)){
         num1 = calc.$1st += e;
         currentDisplay.textContent = num1
         console.table(calc);
     }
-    if (!opCheck(calc.$op && !isEmpty(calc.$1st))){
+    if (!operatorEmpty(calc.$op) && !isEmpty(calc.$1st)){
         num2 = calc.$2nd +=e;
         currentDisplay.textContent = num2
         console.table(calc);
     }
+
+    if(!operatorEmpty(calc.$op) && !isEmpty(calc.$2nd)){
+        delete calc.$2nd;
+        calc.$2nd = e.toString();
+        currentDisplay.textContent = calc.$2nd;
+        calcEquals();
+        console.table(calc);
+
+
+    }
+    if (calc.$1st =="00"){
+        calc.$1st = ''
+        currentDisplay.textContent= '0'
+        console.table(calc);
+    }
+    if (calc.$2nd =="00"){
+        calc.$2nd = ''
+        currentDisplay.textContent= "0"
+        console.table(calc);
+    }
+    displaySplitter()
+    
+}
+
+//prevents multiple decimal insertion
+function decCount(str,x){
+    return (str.split(x)).length - 1;
+}
+
+//adds a decimal
+function decInput(){
+    e='.'
+
+    if (decCount(calc.$2nd,".")===1){
+        return
+    }
+    if (decCount(calc.$1st,".")===1 && isEmpty(calc.$2nd)){
+        return
+    }
+    if (operatorEmpty(calc.$op) && isEmpty(calc.$2nd)){
+        num1 = calc.$1st += e;
+        currentDisplay.textContent = num1
+        console.table(calc);
+    }
+    if (!operatorEmpty(calc.$op) && !isEmpty(calc.$1st)){
+        num2 = calc.$2nd +=e;
+        currentDisplay.textContent = num2
+        console.table(calc);
+    }
+    displaySplitter()
+
 }
 
 
+// POSITIVE OR NEGATIVE KEY
 
+function posOrNeg(){
+    if (operatorEmpty(calc.$op) && isEmpty(calc.$2nd)){
+        if(parseFloat(calc.$1st)>0){
+            num1 = calc.$1st.padStart(calc.$1st.length+1,'-')
+            currentDisplay.textContent=num1;
+            calc.$1st=num1;
+            console.table(calc);
+        }
+        else{
+        num1 = calc.$1st.slice(1);
+        currentDisplay.textContent=num1;
+        calc.$1st = num1
+        console.table(calc)
+        }
+    }
+    if (!operatorEmpty(calc.$op) && !isEmpty(calc.$1st)){
+        if(parseFloat(calc.$2nd)>0){
+            num2 = calc.$2nd.padStart(calc.$2nd.length+1,'-')
+            currentDisplay.textContent=num2;
+            calc.$2nd=num2;
+            console.table(calc);
+        }
+        else{
+        num2 = calc.$2nd.slice(1);
+        currentDisplay.textContent=num2;
+        calc.$2nd = num2
+        console.table(calc)
+        }
+    }
 
-//add event listeners
+}
+
+//percentage function
+
+function percentFunc(){
+    if (operatorEmpty(calc.$op) && isEmpty(calc.$2nd)){
+        calc.$1st = num1 = (parseFloat(calc.$1st)/100).toString()
+        currentDisplay.textContent = num1
+        console.table(calc);
+    }
+    if (!operatorEmpty(calc.$op) && !isEmpty(calc.$1st)){
+        calc.$2nd= num2 = (parseFloat(calc.$2nd)/100).toString()
+        currentDisplay.textContent = num2
+        console.table(calc);
+}
+        displaySplitter()
+}
+
+//add event listeners here
 clear.addEventListener('click',calcClear);
-//plus minus event listener
-//percentage event listener
-divide.addEventListener('click',calcDiv);
-multiply.addEventListener('click',calcMulti);
-subtract.addEventListener('click',calcSubtract);
-add.addEventListener('click',calcAdd);
-//decimal event listener
-one.addEventListener('click',numInput(1))
-two.addEventListener('click',numInput('2'))
-three.addEventListener('click',numInput('3'))
-four.addEventListener('click',numInput('4'))
-five.addEventListener('click',numInput('5'))
-six.addEventListener('click',numInput('6'))
-seven.addEventListener('click',numInput('7'))
-eight.addEventListener('click',numInput('8'))
-nine.addEventListener('click',numInput('9'))
-zero.addEventListener('click',numInput('0'))
+plusminus.addEventListener('click',posOrNeg);
+percentage.addEventListener('click',percentFunc)
+divide.addEventListener('click',function(){calc.$op=calcDiv});
+multiply.addEventListener('click',function(){calc.$op=calcMulti});
+subtract.addEventListener('click',function(){calc.$op=calcSubtract});
+add.addEventListener('click',function(){calc.$op=calcAdd});
+decimal.addEventListener('click',decInput);
+one.addEventListener('click',numInput);
+two.addEventListener('click',numInput);
+three.addEventListener('click',numInput);
+four.addEventListener('click',numInput);
+five.addEventListener('click',numInput);
+six.addEventListener('click',numInput);
+seven.addEventListener('click',numInput);
+eight.addEventListener('click',numInput);
+nine.addEventListener('click',numInput);
+zero.addEventListener('click',numInput);
+equals.addEventListener('click',calcEquals);
+
+document.addEventListener('keydown', (e)=>{
+if (e.key.match(keyRegEx)){
+keyNumInput(e.key)}}
+)
+
+/////KEYBOARD LISTENER//////
+/////Keyboard Num Input////
+function keyNumInput(x){
+    e=x
+    if (operatorEmpty(calc.$op) && isEmpty(calc.$2nd)){
+        num1 = calc.$1st += e;
+        currentDisplay.textContent = num1
+        console.table(calc);
+    }
+    if (!operatorEmpty(calc.$op) && !isEmpty(calc.$1st)){
+        num2 = calc.$2nd +=e;
+        currentDisplay.textContent = num2
+        console.table(calc);
+    }
+    if (calc.$1st =="00"){
+        calc.$1st = ''
+        currentDisplay.textContent= '0'
+        console.table(calc);
+    }
+    if (calc.$2nd =="00"){
+        calc.$2nd = ''
+        currentDisplay.textContent= "0"
+        console.table(calc);
+    }
+    displaySplitter()
+    
+}
+
+//equals//
+document.addEventListener('keydown', (e)=>{
+    if (e.key=="Enter"){
+   calcEquals();
+    }}
+    )
+
+//add//
+document.addEventListener('keydown', (e)=>{
+    if (e.key=="+"){
+    calc.$op=calcAdd
+    }}
+    )
+
+//subtract//
+
+document.addEventListener('keydown', (e)=>{
+    if (e.key=="-"){
+    calc.$op=calcSubtract
+    }}
+    )
+
+//multiply//
+document.addEventListener('keydown', (e)=>{
+    if (e.key=="*"){
+    calc.$op=calcMulti
+    }}
+    )
+
+//divide//
+document.addEventListener('keydown', (e)=>{
+    if (e.key=="/"){
+    calc.$op=calcDiv
+    }}
+    )
+
+//clear//
+document.addEventListener('keydown', (e)=>{
+    if (e.key=="Delete"||e.key=="Backspace"){
+    calcClear()
+    }}
+    )
+
+
+//decimal//
+document.addEventListener('keydown', (e)=>{
+    if (e.key=="."||e.key=="Backspace"){
+    decInput();
+    }}
+    )
